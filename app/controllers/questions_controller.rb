9b25edf2c8b01e0a2780acfc6b7dbab0f3468133
Chafique ABIBOURAGUIMANE <shafiq.82@outlook.com>
2024-11-11 14:23:50 +0900

    Stupid coaching with rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def answer
+    @answer = "Bof!";
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!";
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!";
+    else
+      @answer = "I don't care, get dressed and go to work!";
+    end
+  end    
+end
